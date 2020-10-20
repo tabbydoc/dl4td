da546e38750a430e5012da151a3a7ba44ac31603
Игорь Черепанов <30562858+ZigorewsLike@users.noreply.github.com>
2020-10-20 15:34:28 +0800

    Google colab fix

--- scripts/image_transform/main.py
+++ scripts/image_transform/main.py
@@ -32,8 +32,6 @@ def main(argv):
 
     if not is_dir:
         raise Exception(input_folder, "- It is not a folder")
-
-    print("= Begin transform =")
     for root, directories, files in os.walk(input_folder):
         for file_name in fnmatch.filter(files, "*.jpeg"):
             path = os.path.join(root, file_name)
@@ -61,10 +59,6 @@ def transform_image(img):
     r = cv2.distanceTransform(img, distanceType=cv2.DIST_C, maskSize=5)
     img = cv2.merge((b, g, r))
     return img
-
-    # img_blur = img.filter(ImageFilter.EMBOSS)
-    # img_gaus = img_blur.filter(ImageFilter.GaussianBlur())
-    # return img_blur
 
 
 if __name__ == "__main__":
